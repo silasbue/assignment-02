1a1c03be913ea576e600c01b765c847a469ab23a
Silas Bue Handgaard <48789220+silasbue@users.noreply.github.com>
2022-09-16 15:15:19 +0200

    Implemented and tested Student class in new libraries (#1)

---
+++ StudentLib.Tests/StudentTests.cs
@@ -0,0 +1,46 @@
+namespace StudentLib.Tests;
+
+public class StudentTests
+{
+  [Fact]
+  public void Student_status_given_Dates_With_Years_2002_2004_2004_returns_Status_Graduated()
+  {
+    Student student = new Student(1, "John", "Doe", new DateTime(2002, 12, 4), new DateTime(2004, 5, 5), new DateTime(2004, 5, 5));
+
+    var result = student.status;
+
+    result.Should().Be(Status.Graduated);
+  }
+
+  [Fact]
+  public void Student_status_given_Dates_With_Years_2021_2023_2023_returns_Status_Active()
+  {
+    Student student = new Student(1, "John", "Doe", new DateTime(2021, 10, 2), new DateTime(2023, 4, 4), new DateTime(2023, 4, 4));
+
+    var result = student.status;
+
+    result.Should().Be(Status.Active);
+  }
+
+  [Fact]
+  public void Student_status_given_Dates_With_Years_2022_2024_2024_returns_Status_New()
+  {
+    Student student = new Student(1, "John", "Doe", new DateTime(2022, 8, 30), new DateTime(2024, 4, 4), new DateTime(2024, 4, 4));
+
+    var result = student.status;
+
+    result.Should().Be(Status.New);
+  }
+
+  [Fact]
+  public void Student_status_given_Dates_With_Years_2020_2021_2024_returns_Status_Dropout()
+  {
+    Student student = new Student(1, "John", "Doe", new DateTime(2020, 8, 30), new DateTime(2021, 4, 4), new DateTime(2024, 4, 4));
+
+    var result = student.status;
+
+    result.Should().Be(Status.Dropout);
+  }
+
+
+}
---
+++ StudentLib/Student.cs
@@ -0,0 +1,53 @@
+﻿namespace StudentLib;
+public class Student
+{
+  public Student(int id, string givenName, string surName, DateTime startDate, DateTime endDate, DateTime graduationDate)
+  {
+    this.id = id;
+    GivenName = givenName;
+    SurName = surName;
+    StartDate = startDate;
+    EndDate = endDate;
+    GraduationDate = graduationDate;
+  }
+
+  public int id { get; init; }
+  public string GivenName { get; set; } = null!;
+  public string SurName { get; set; } = null!;
+
+  public Status status
+  {
+    get
+    {
+      var date = DateTime.Now;
+      if (date > StartDate && date < StartDate.AddMonths(2))
+      {
+        return Status.New;
+      }
+      else if (date > StartDate && date < EndDate)
+      {
+        return Status.Active;
+      }
+      else if (date > EndDate && EndDate < GraduationDate)
+      {
+        return Status.Dropout;
+      }
+      else
+      {
+        return Status.Graduated;
+      }
+    }
+  }
+
+  public DateTime StartDate { get; set; }
+  public DateTime EndDate { get; set; }
+  public DateTime GraduationDate { get; set; }
+}
+
+public enum Status
+{
+  New,
+  Active,
+  Dropout,
+  Graduated
+}
