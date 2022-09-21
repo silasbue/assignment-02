3faf3c80974f8bf9df2781a4bfbdbebe6da83efb
Silas Bue Handgaard <silasbueh@hotmail.com>
2022-09-21 17:30:56 +0200

    Implemented and tested isSecure extension method

--- Assignment2.Tests/ExtensionsTests.cs
+++ Assignment2.Tests/ExtensionsTests.cs
@@ -2,9 +2,23 @@ namespace Assignment2.Tests;
 
 public class ExtensionsTests
 {
-    [Fact]
-    public void Test1()
-    {
+  [Fact]
+  public void isSecure_given_https_uri_returns_true()
+  {
+    var input = new Uri("https://www.google.com/");
 
-    }
+    var result = Extensions.isSecure(input);
+
+    result.Should().Be(true);
+  }
+
+  [Fact]
+  public void isSecure_given_http_uri_returns_false()
+  {
+    var input = new Uri("http://www.google.com/");
+
+    var result = Extensions.isSecure(input);
+
+    result.Should().Be(false);
+  }
 }
--- Assignment2/Extensions.cs
+++ Assignment2/Extensions.cs
@@ -2,4 +2,6 @@ namespace Assignment2;
 
 public static class Extensions
 {
+  public static bool isSecure(Uri input) => input.Scheme == "https";
+
 }
