d2b7d9baf5cb14d50cbb540d499d53b7ee9850ec
Silas Bue Handgaard <silasbueh@hotmail.com>
2022-09-21 21:48:59 +0200

    Started linq queries tests

--- Assignment2.Tests/QueriesTests.cs
+++ Assignment2.Tests/QueriesTests.cs
@@ -2,9 +2,28 @@ namespace Assignment2.Tests;
 
 public class QueriesTests
 {
-    [Fact]
-    public void Test1()
-    {
+  [Fact]
+  public void Test1()
+  {
+    var wizards = WizardCollection.Create();
 
-    }
+    var result = from wizard in wizards
+                 where wizard.Creator == "Rowling"
+                 select wizard.Name;
+
+    result.Should().BeEquivalentTo(new List<string> { "Harry Potter" });
+  }
+
+  [Fact]
+  public void Test2()
+  {
+    var wizards = WizardCollection.Create();
+
+    var result = from wizard in wizards
+                 where wizard.Name.StartsWith("Darth")
+                 group wizard by true into siths
+                 select siths.Min(s => s.Year);
+
+    result.Should().BeEquivalentTo(new List<int> { 1977 });
+  }
 }
